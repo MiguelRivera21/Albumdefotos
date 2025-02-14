53a79553e2fcb0d7b7f9ded9a6a0e02328ed3c22
MiguelRivera21 <21miguelrivera@micorreo.upp.edu.mx>
2025-02-14 08:42:49 -0600

    RESPONSIVE2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const photoContainers = document.querySelectorAll('.photo-container');
     const closeBtns = document.querySelectorAll('.close-btn');
+    const body = document.body;
 
     // Abrir modal al hacer clic en una foto
     photoContainers.forEach(container => {
@@ -8,6 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
             const modalId = container.getAttribute('data-modal');
             const modal = document.getElementById(modalId);
             modal.style.display = 'flex';
+            body.style.overflow = 'hidden'; // Evita el desplazamiento del fondo
         });
     });
 
@@ -16,6 +18,7 @@ document.addEventListener("DOMContentLoaded", function() {
         btn.addEventListener('click', () => {
             const modal = btn.closest('.modal');
             modal.style.display = 'none';
+            body.style.overflow = 'auto'; // Restaura el desplazamiento del fondo
         });
     });
 
@@ -23,6 +26,7 @@ document.addEventListener("DOMContentLoaded", function() {
     document.addEventListener('click', (e) => {
         if (e.target.classList.contains('modal')) {
             e.target.style.display = 'none';
+            body.style.overflow = 'auto'; // Restaura el desplazamiento del fondo
         }
     });
 });
